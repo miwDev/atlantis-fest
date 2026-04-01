f783a56110428972be1c7aca5b05ba4d2bf69518
msd.dev <msierradiego00@gmail.com>
2026-04-01 19:59:34 +0200

    Feat(Project creation) initial commit 0.0.1

---
+++ src/main/java/com/msd/atlantis_fest/AtlantisFestApplication.java
@@ -0,0 +1,13 @@
+package com.msd.atlantis_fest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlantisFestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlantisFestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msd/atlantis_fest/AtlantisFestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msd.atlantis_fest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlantisFestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
