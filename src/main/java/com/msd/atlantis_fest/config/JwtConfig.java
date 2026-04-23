ce6a507f03673c883daadd6d91e3d592073f13d3
msd.dev <57422526+miwDev@users.noreply.github.com>
2026-04-23 15:54:47 +0200

    Feat(JWTclasses, appProperties): cambios en el properties para lectura de mi ficjero .env, cambios en jwt minor big fix

--- src/main/java/com/msd/atlantis_fest/config/JwtConfig.java
+++ src/main/java/com/msd/atlantis_fest/config/JwtConfig.java
@@ -10,8 +10,8 @@ import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class JwtConfig {
-
-    @Value("${JWT_SECRET}")
+    
+    @Value("${JWT_SECRET:8a4d46c83f99b22b10a955db3f0556218d601b0f70c32607b2f6b4d3f57279c2}")
     private String jwtSecret;
 
     @Bean
