d5ba3bf4ba0d1170a859dedf7463e74a5b3f56b1
miguelS <miguel.sierra@nter.es>
2026-04-23 11:39:31 +0200

    Feat(User details implementation): implementado repositorio y servicio con user details para lo relaccionado con jwt

---
+++ src/main/java/com/msd/atlantis_fest/service/impl/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.msd.atlantis_fest.service.impl;
+
+import com.msd.atlantis_fest.entity.User;
+import com.msd.atlantis_fest.repository.UserRepository;
+import com.msd.atlantis_fest.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserDetailsService, UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User no encontrado por name: " + username));
+    }
+}
